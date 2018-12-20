74b53e2358c5b2674f76c60f0cdec193706b1cb2
SunCong <suncongbj@gmail.com>
2018-12-20 16:12:50 +0800

    no message

--- blog-api/app.js
+++ blog-api/app.js
@@ -5,11 +5,11 @@ const Koa = require('koa');
 const app = new Koa();
 
 // 对于任何请求，app将调用该异步函数处理请求：
-async (ctx, next) => {
-    ctx.render('./index.html', {
-        title: 'Welcome'
-    });
-}
+app.use(async (ctx, next) => {
+    await next();
+    ctx.response.type = 'text/html';
+    ctx.response.body = '<h1>Hello, koa2!</h1>';
+});
 
 // 在端口3000监听:
 app.listen(80);
